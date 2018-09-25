78b3419c9eb61785cf9d0644e310a9500ee8924b
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-25 20:26:13 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- CodeCamp_Percolation/Solution.java
+++ CodeCamp_Percolation/Solution.java
@@ -5,12 +5,24 @@ class Percolation {
     // private int[] arr_1;
     private int size;
 
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     */
+
     public Percolation(int n) {
         arr_2 = new boolean[n][n];
         // arr_1 = new int[n*n];
         u = new WeightedQuickUnionUF(n * n + 2);
         size = n;
     }
+    /**
+     * { function_description }.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
 
     public void open(int row, int col) {
         if (arr_2[row][col]) return;
@@ -37,18 +49,39 @@ class Percolation {
             u.union(row * size + col, (size * size) + 1);
         }
     }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
 
     public boolean isOpen(int row, int col) {
         return arr_2[row][col];
 
     }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
 
     public boolean percolates() {
         return u.connected(size * size, size * size + 1);
     }
 }
+/**
+ * { item_description }.
+ */
 
 public final class Solution {
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments
+     */
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
