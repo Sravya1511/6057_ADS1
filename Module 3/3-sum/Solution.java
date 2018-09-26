a375a1e9b6cc8491ed1fcd7fe4cda78ecc6ef4e4
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 22:48:48 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -4,50 +4,50 @@ import java.util.Arrays;
  * input reading.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**
-		 * { var_description }
-		 */
-		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
-		int[] array;
-		array = new int[n];
-		for (int i = 0; i < n; i++) {
-			array[i] = input.nextInt();
-		}
-		Arrays.sort(array);
-		threesum(array, n);
-	}
-	/**
-	 * {three sum function.
-	 *
-	 * @param      array  The array
-	 * @param      n      { parameter_description }
-	 */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] array;
+        array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+        }
+        Arrays.sort(array);
+        threesum(array, n);
+    }
+    /**
+     * {three sum function.
+     *
+     * @param      array  The array
+     * @param      n      { parameter_description }
+     */
 
-	public static void threesum(final int[] array, final int n) {
-		// System.out.println(Arrays.toString(array));
+    public static void threesum(final int[] array, final int n) {
+        // System.out.println(Arrays.toString(array));
         int count = 0;
-		for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int k = Arrays.binarySearch(array, -(array[i] + array[j]));
                 if (k > j) {
-                	count++;
+                    count++;
                 }
             }
         }
 
-		System.out.println(count);
-	}
+        System.out.println(count);
+    }
 }
