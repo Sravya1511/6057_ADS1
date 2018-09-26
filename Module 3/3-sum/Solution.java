7bf90c269cc9bbcc4310e9dab2918deed5d5f1b0
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 17:10:08 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -19,10 +19,10 @@ class Solution {
 		for(int i = 0; i<n-2; i++) {
 			// System.out.println("a");
 			for(int j = i+1; j<n-1; j++) {
-				long sum = 0;
+				// long sum = 0;
 				// System.out.println(sum);
 				for(int k = j+1; k<n; k++) {
-					sum = array[i]+array[j]+array[k];
+					long sum = array[i]+array[j]+array[k];
 					// System.out.println(sum);
 					if(sum == 0) {
 						count++;
