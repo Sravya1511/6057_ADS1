ca2dcac407e592d3fcb8e90802c9894f61651c81
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 22:45:35 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -1,26 +1,25 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 class Solution {
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		int [] array;
 		array = new int[n];
-		for(int i= 0; i < n; i++) {
+		for(int i = 0; i < n; i++) {
 			array[i] = input.nextInt();
 		}
 		Arrays.sort(array);
 		threesum(array, n);
 	}
 
-	public static void threesum(int[] array, int n) {
+	public static void threesum(final int[] array, final int n) {
 		// System.out.println(Arrays.toString(array));
         int count = 0;
 		for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 int k = Arrays.binarySearch(array, -(array[i] + array[j]));
-                // if (k > j) StdOut.println(a[i] + " " + a[j] + " " + a[k]);
-                // int sum = array[i] + array[j] + k;
-                if (k>j) {
+                if (k > j) {
                 	count++;
                 }
             }
