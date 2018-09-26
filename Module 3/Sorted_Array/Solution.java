4cc0eb9561d91785ae508a67bad1cbbd562bdbfd
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 20:45:46 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- Module 3/Sorted_Array/Solution.java
+++ Module 3/Sorted_Array/Solution.java
@@ -13,11 +13,11 @@ class Solution {
 		Scanner input = new Scanner(System.in);
 		int a = Integer.parseInt(input.nextLine());
 		int b = Integer.parseInt(input.nextLine());
-		String [] t = input.nextLine().split(",");
-		String [] s = input.nextLine().split(",");
-		int [] array1;
+		String[] t = input.nextLine().split(",");
+		String[] s = input.nextLine().split(",");
+		int[] array1;
 
-		int [] array2;
+		int[] array2;
 		array2 = new int[s.length];
 
 		if (t[0].equals("")) {
@@ -51,21 +51,20 @@ class Solution {
 		int j = 0;
 		int [] result;
 		result = new int[array1.length + array2.length];
-		for (int k = 0; k<result.length; k++) {
-			if (i<array1.length && j<array2.length) {
+		for (int k = 0; k < result.length; k++) {
+			if (i < array1.length && j < array2.length) {
 				if (array1[i] < array2[j]) {
 					result[k] = array1[i++];
-				}
-				else
+				} else {
 					result[k] = array2[j++];
-			}
-			else if (i >= array1.length) {
+				}
+			} else if (i >= array1.length) {
 				result[k] = array2[j++];
 			} else if (j >= array2.length) {
 				result[k] = array1[i++];
 			}
 		}
-		System.out.println(Arrays.toString(result).replace("[", "").replace("]","").replace(" ",""));
+		System.out.println(Arrays.toString(result).replace("[", "").replace("]", "").replace(" ", ""));
 	}
 
 }
