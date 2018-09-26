059848840ca97aa6a45a96ffbcec618ebf035ad7
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 21:46:30 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -3,8 +3,8 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
-		long [] array;
-		array = new long[n];
+		int [] array;
+		array = new int[n];
 		for(int i= 0; i<n; i++) {
 			array[i] = input.nextInt();
 		}
@@ -12,26 +12,20 @@ class Solution {
 		threesum(array, n);
 	}
 
-	public static void threesum(long[] array, int n) {
+	public static void threesum(int[] array, int n) {
 		// System.out.println(Arrays.toString(array));
+        int count = 0;
+		for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int k = Arrays.binarySearch(array, -(array[i] + array[j]));
+                // if (k > j) StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+                int sum = array[i] + array[j]+k;
+                if(sum == 0) {
+                	count++;
+                }
+            }
+        }
 
-		int count = 0;
-		for(int i = 0; i<n-2; i++) {
-			// System.out.println("a");
-			for(int j = i+1; j<n-1; j++) {
-				long sum = 0;
-				// System.out.println(sum);
-				for(int k = j+1; k<n; k++) {
-					sum = array[i]+array[j]+array[k];
-					// System.out.println(sum);
-					if(sum == 0) {
-						count++;
-						// break;
-					}
-				}
-			}
-
-		}
 		System.out.println(count);
 	}
 }
