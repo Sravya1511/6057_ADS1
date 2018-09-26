a9187eac98a82dd1fe240723f15bf4b81b7faa9a
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 15:20:05 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- CodeCamp_Percolation/Codecamp.java
+++ CodeCamp_Percolation/Codecamp.java
@@ -28,7 +28,7 @@ class Percolation {
 		// System.out.println(Arrays.toString(arr_1));
     }
       // open site (row, col) if it is not open already
-      public void open(int row, int col, int n)   {
+      public void open(int row, int col)   {
       	arr_2[row-1][col-1] = 1;
       	int i = row-1;
       	int j = col-1;
@@ -51,25 +51,30 @@ class Percolation {
     	for(int i = 0; i<n; i++) {
     		for(int j = 0; j<n; j++) {
     			if(arr_2[i][j] == 1) {
+                    //right
     				if (j < n-1) {
     					if(arr_2[i][j+1] == 1){
     						union(i*n+j, i*n+j+1);
     					}
     				}
+                    //bottom
     				if (i < n-1) {
                         if(arr_2[i+1][j] == 1) {
     						union(i*n+j, (i+1)*n+j);
     					}
     				}
+
+                    //top
     				if(i>0) {
     					if(arr_2[i-1][j] == 1) {
     						union(i*n+j, (i-1)*n+j);
     					}
     				}
 
+                    //left
     				if(j>0) {
     					if(arr_2[i][j-1] == 1) {
-    						union(i*n+j, i*n+j-1);
+    						union(i*n+j, i*n+(j-1));
     					}
     				}
 
@@ -91,20 +96,28 @@ class Percolation {
     // }
 
     public boolean percolates(int n) {
-    	int i = 0;
-    	int j = n*n-n;
-    	while(i<n && j<n*n) {
-    		// System.out.println("h");
+    	// int i = 0;
+    	// int j = n*n-n;
+    	// while(i<n && j<n*n) {
+        for(int i = 0; i<n; i++) {
+            for(int j = n*n-n; j<n*n; j++) {
+                if (root(arr_1[i]) == root(arr_1[j])) {
+                // System.out.println("i");
+                return true;
+            }
+            }
+        }
+    		// // System.out.println("h");
 
-    		if (root(arr_1[i]) == root(arr_1[j])) {
-    			// System.out.println("i");
-    			return true;
-    		}
-    		else {
-    			i++;
-    			j++;
-    		}
-    	}
+    		// if (root(arr_1[i]) == root(arr_1[j])) {
+    		// 	// System.out.println("i");
+    		// 	return true;
+    		// }
+    		// else {
+    		// 	i++;
+    		// 	j++;
+    		// }
+
     	return false;
 
     }
@@ -128,12 +141,12 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = Integer.parseInt(input.nextLine());
-		u.WeightedQuickUnionUF(n);
+		// u.WeightedQuickUnionUF(n);
 		Percolation p = new Percolation(n);
 		p.percolation(n);
 	while(input.hasNext()) {
 		String[] token = input.nextLine().split(" ");
-		p.open(Integer.parseInt(token[0]), Integer.parseInt(token[1]), n);
+		p.open(Integer.parseInt(token[0]), Integer.parseInt(token[1]));
 	}
 		p.solution(n);
 		System.out.println(p.percolates(n));
