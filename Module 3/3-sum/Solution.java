b2bfa6cb05de76a126bb079e3cacfa41f7401eed
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 22:47:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
+/**
+ * input reading
+ */
+final class Solution {
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
+		/**
+		 * { var_description }
+		 */
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
-		int [] array;
+		int[] array;
 		array = new int[n];
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			array[i] = input.nextInt();
 		}
 		Arrays.sort(array);
 		threesum(array, n);
 	}
+	/**
+	 * {three sum function
+	 *
+	 * @param      array  The array
+	 * @param      n      { parameter_description }
+	 */
 
 	public static void threesum(final int[] array, final int n) {
 		// System.out.println(Arrays.toString(array));
