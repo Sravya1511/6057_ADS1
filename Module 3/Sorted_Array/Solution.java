6c41a2af7e1f14bee0b52d54b4a6fdf283974c35
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 20:43:44 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- Module 3/Sorted_Array/Solution.java
+++ Module 3/Sorted_Array/Solution.java
@@ -1,27 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		int a = Integer.parseInt(input.nextLine());
 		int b = Integer.parseInt(input.nextLine());
-		String[] t = input.nextLine().split(",");
-		String[] s = input.nextLine().split(",");
-		int[] array1;
+		String [] t = input.nextLine().split(",");
+		String [] s = input.nextLine().split(",");
+		int [] array1;
 
-		int[] array2;
+		int [] array2;
 		array2 = new int[s.length];
 
-		if(t[0].equals("")) {
+		if (t[0].equals("")) {
 			array1 = new int[0];
-		}
-		else {
+		} else {
 			array1 = new int[t.length];
-			for(int i = 0; i<t.length; i++) {
+			for (int i = 0; i < t.length; i++) {
 			array1[i] = Integer.parseInt(t[i]);
 		}
 
 		}
-		for(int i = 0; i<s.length; i++) {
+		for (int i = 0; i < s.length; i++) {
 			array2[i] = Integer.parseInt(s[i]);
 		}
 
@@ -31,24 +39,29 @@ class Solution {
 		sort(array1, array2);
 
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      array1  The array 1
+	 * @param      array2  The array 2
+	 */
 
-	public static void sort(int[] array1, int[] array2) {
+	public static void sort(final int[] array1, final int[] array2) {
 		int i = 0;
 		int j = 0;
 		int [] result;
-		result = new int[array1.length+array2.length];
-		for(int k = 0; k<result.length; k++) {
-			if(i<array1.length && j<array2.length) {
-				if(array1[i] < array2[j]) {
+		result = new int[array1.length + array2.length];
+		for (int k = 0; k<result.length; k++) {
+			if (i<array1.length && j<array2.length) {
+				if (array1[i] < array2[j]) {
 					result[k] = array1[i++];
 				}
 				else
 					result[k] = array2[j++];
 			}
-			else if(i>=array1.length) {
+			else if (i >= array1.length) {
 				result[k] = array2[j++];
-			}
-			else if(j>=array2.length) {
+			} else if (j >= array2.length) {
 				result[k] = array1[i++];
 			}
 		}
