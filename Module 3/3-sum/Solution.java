43284e91d6ef5bc6f1441a35e3f888eaa89bf7cc
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 14:56:00 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ Module 3/3-sum/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class solution {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int [] array;
+		array = new int[n];
+		for(int i= 0; i<n; i++) {
+			array[i] = input.nextInt();
+		}
+		Arrays.sort(array);
+		threesum(array, n);
+	}
+
+	public static void threesum(int[] array, int n) {
+		System.out.println(Arrays.toString(array));
+
+		int count = 0;
+		for(int i = 0; i<n-2; i++) {
+			// System.out.println("a");
+			for(int j = i+1; j<n-1; j++) {
+				int sum = 0;
+				// System.out.println(sum);
+				for(int k = j+1; k<n; k++) {
+					sum = array[i]+array[j]+array[k];
+					// System.out.println(sum);
+					if(sum == 0) {
+						count++;
+						break;
+					}
+				}
+			}
+
+		}
+		System.out.println(count);
+	}
+}
