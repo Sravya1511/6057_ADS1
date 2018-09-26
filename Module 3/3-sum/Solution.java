a8afcfc09eb7abd3981dd96e527516b876331b09
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-26 22:39:39 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module 3/3-sum/Solution.java
+++ Module 3/3-sum/Solution.java
@@ -11,45 +11,16 @@ class Solution {
 		Arrays.sort(array);
 		threesum(array, n);
 	}
-	public static int binarySearch(int arr[], int l, int r, int x)
-    {
-        if (r>=l)
-        {
-            int mid = l + (r - l)/2;
 
-            // If the element is present at the
-            // middle itself
-            if (arr[mid] == x)
-               return mid;
-
-            // If element is smaller than mid, then
-            // it can only be present in left subarray
-            if (arr[mid] > x)
-               return binarySearch(arr, l, mid-1, x);
-
-            // Else the element can only be present
-            // in right subarray
-            return binarySearch(arr, mid+1, r, x);
-        }
-
-        // We reach here when element is not present
-        //  in array
-        return -1;
-    }
 	public static void threesum(int[] array, int n) {
 		// System.out.println(Arrays.toString(array));
         int count = 0;
 		for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
-                // int k = binarySearch(array, -(array[i] + array[j]));
+                int k = Arrays.binarySearch(array, -(array[i] + array[j]));
                 // if (k > j) StdOut.println(a[i] + " " + a[j] + " " + a[k]);
                 // int sum = array[i] + array[j] + k;
-                int a = array[i] + array[j];
-                int k = binarySearch(array, 0, n, -a);
-                // if (k>j) {
-                // 	count++;
-                // }
-                if (a+k == 0) {
+                if (k>j) {
                 	count++;
                 }
             }
