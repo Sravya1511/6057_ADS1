6ed3d747296a9e47f5a1d1cc412f9c4e6a25dc46
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-27 16:20:13 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module 4/4.1 Balanced Paranthesis/Stack.java
+++ Module 4/4.1 Balanced Paranthesis/Stack.java
@@ -1,94 +1,139 @@
-import java.util.Stack;
-import java.util.Scanner;
-import java.util.Arrays;
+// import java.util.Stack;
+// import java.util.Scanner;
+// import java.util.Arrays;
 
-class StackArray {
-	String[] array;
-	int size;
+// class Test {
+// 	public static void main(String[] args) {
+// 		Scanner scn = new Scanner(System.in);
+// 		int lines = Integer.parseInt(scn.nextLine());
+// 		for(int i = 0; i<lines; i++) {
+// 			String[] tokens = scn.nextLine().split("");
+// 			paranthesis(tokens);
+// 		}
+// 	}
 
-	StackArray(int n) {
-		array = new String[n];
-		size = 0;
-	}
+// 	public static void paranthesis(String[] tokens) {
+// 		boolean x = false;
+// 		Stack<String> str = new Stack<>();
+// 		for(int j = 0; j<tokens.length; j++) {
+// 		    if(tokens[j].equals("}") || tokens[j].equals("]") || tokens[j].equals(")")) {
 
-	public void push(String item) {
-		array[size++] = item;
-	}
+// 		        if(str.isEmpty()) {
+// 		        	System.out.println("NO");
+// 		        	x = true;
+// 		        	break;
 
-	public void pop() {
-		array[size-1] = null;
-		size--;
-	}
-	public boolean isEmpty() {
-		if (size == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	public String peek() {
-		int a = size-1;
-		return array[a];
-	}
-}
+// 		        	}
+// 				str.pop();
+// 			    }
+// 			    else{
+// 			        str.push(tokens[j]);
+// 			    }
+// 			}
+//             // System.out.println(str);
+//             if(!x) {
+//             	if(str.isEmpty()) {
+//             	System.out.println("YES");
+//             }
+//             else {
+//             	System.out.println("NO");
+//             }
+//             }
+// 		}
+// 	}
 
-class Solution {
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		int lines = Integer.parseInt(scn.nextLine());
-		for(int i = 0; i<lines; i++) {
-			String[] tokens = scn.nextLine().split("");
-			paranthesis(tokens);
-		}
-	}
 
-	public static void paranthesis(String[] tokens) {
-		boolean x = false;
-		String i = ")";
-		String j = "]";
-		String k = "}";
-		StackArray str = new StackArray(tokens.length);
-		for(int m = 0; m<tokens.length; m++) {
-		    if(tokens[m].equals(i) || tokens[m].equals(j) || tokens[m].equals(k)) {
-		        if(str.isEmpty()) {
-		        	System.out.println("NO");
-		        	x = true;
-		        	break;
-		        }
-		        String a = str.peek();
-		        // System.out.println(a);
-		        if(a.equals("(") && (tokens[m].equals("]") || tokens[m].equals("}"))) {
-		        	System.out.println("NO");
-		        	x = true;
-		        	break;
-		        }
-		        if(a.equals("{") && (tokens[m].equals("]") || tokens[m].equals(")"))) {
-		        	System.out.println("NO");
-		        	x = true;
-		        	break;
-		        }
-		        if(a.equals("[") && (tokens[m].equals(")") || tokens[m].equals("}"))) {
-		        	System.out.println("NO");
-		        	x = true;
-		        	break;
-		        }
-		        str.pop();
-			}
-			    else{
-			        str.push(tokens[m]);
-			    }
-			}
-            // System.out.println(str);
-            if(!x) {
-            	if(str.isEmpty()) {
-            	System.out.println("YES");
-            }
-            else {
-            	System.out.println("NO");
-            }
-            }
+// import java.util.Stack;
+// import java.util.Scanner;
+// import java.util.Arrays;
 
-		}
+// class StackArray {
+// 	String[] array;
+// 	int size;
 
-	}
+// 	StackArray(int n) {
+// 		array = new String[n];
+// 		size = 0;
+// 	}
+
+// 	public void push(String item) {
+// 		array[size++] = item;
+// 	}
+
+// 	public void pop() {
+// 		array[size-1] = null;
+// 		size--;
+// 	}
+// 	public boolean isEmpty() {
+// 		if (size == 0) {
+// 			return true;
+// 		}
+// 		else {
+// 			return false;
+// 		}
+// 	}
+// 	public String peek() {
+// 		int a = size-1;
+// 		return array[a];
+// 	}
+// }
+
+// class Solution {
+// 	public static void main(String[] args) {
+// 		Scanner scn = new Scanner(System.in);
+// 		int lines = Integer.parseInt(scn.nextLine());
+// 		for(int i = 0; i<lines; i++) {
+// 			String[] tokens = scn.nextLine().split("");
+// 			paranthesis(tokens);
+// 		}
+// 	}
+
+// 	public static void paranthesis(String[] tokens) {
+// 		boolean x = false;
+// 		String i = ")";
+// 		String j = "]";
+// 		String k = "}";
+// 		StackArray str = new StackArray(tokens.length);
+// 		for(int m = 0; m<tokens.length; m++) {
+// 		    if(tokens[m].equals(i) || tokens[m].equals(j) || tokens[m].equals(k)) {
+// 		        if(str.isEmpty()) {
+// 		        	System.out.println("NO");
+// 		        	x = true;
+// 		        	break;
+// 		        }
+// 		        String a = str.peek();
+// 		        // System.out.println(a);
+// 		        if(a.equals("(") && (tokens[m].equals("]") || tokens[m].equals("}"))) {
+// 		        	System.out.println("NO");
+// 		        	x = true;
+// 		        	break;
+// 		        }
+// 		        if(a.equals("{") && (tokens[m].equals("]") || tokens[m].equals(")"))) {
+// 		        	System.out.println("NO");
+// 		        	x = true;
+// 		        	break;
+// 		        }
+// 		        if(a.equals("[") && (tokens[m].equals(")") || tokens[m].equals("}"))) {
+// 		        	System.out.println("NO");
+// 		        	x = true;
+// 		        	break;
+// 		        }
+// 		        str.pop();
+// 			}
+// 			    else{
+// 			        str.push(tokens[m]);
+// 			    }
+// 			}
+//             // System.out.println(str);
+//             if(!x) {
+//             	if(str.isEmpty()) {
+//             	System.out.println("YES");
+//             }
+//             else {
+//             	System.out.println("NO");
+//             }
+//             }
+
+// 		}
+
+// 	}
