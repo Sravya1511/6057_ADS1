c7f98f43c6ad33d7eeeeae2b919c23188c1abd45
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-09-30 19:26:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- Module 5/5.2 Josephus Problem/Solution.java
+++ Module 5/5.2 Josephus Problem/Solution.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
-
-
 /**
  * Solution class.
  */
 public final class Solution {
-
+    /**
+     * Constructs the object.
+     */
 
     private Solution() {
     }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        /**
+         * reads input.
+         */
         Scanner input = new Scanner(System.in);
         int n = Integer.parseInt(input.nextLine());
-        for(int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             CircularQueue counter = new CircularQueue();
             String[] tokens = input.nextLine().split(" ");
             int number = Integer.parseInt(tokens[0]);
-            for(int j = 0; j<number; j++) {
+            for (int j = 0; j < number; j++) {
                 counter.insert(j);
             }
             String string = "";
