eee9a906b2c2ce057baad2d303ff43ff9ab98abf
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-01 10:50:24 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ Module 7/7.1 Steque/Solution.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+class LinkedList {
+    Node head = null;
+    // Node tail;
+    int size = 0;
+
+	class Node {
+		int data;
+		Node next;
+
+	Node(int data) {
+		this.data = data;
+		next = null;
+	}
+}
+
+	public void push(int data) {
+		size++;
+		Node n = new Node(data);
+		if(head == null) {
+			head = n;
+		}
+		else{
+			n.next = head;
+			head = n;
+		}
+	}
+
+	public void enqueue(int data) {
+		size++;
+		Node last = new Node(data);
+		if(head == null) {
+			head = last;
+			return;
+		}
+		Node n = head;
+		while(n.next!=null) {
+			n = n.next;
+		}
+		n.next = last;
+	}
+
+	public boolean pop() {
+		if(size == 0) {
+			return false;
+		}
+		else {
+		    head = head.next;
+		    size--;
+		    return true;
+		}
+	}
+
+	public void print() {
+		Node n = head;
+		String str = "";
+		while(n!=null) {
+			str += n.data+", ";
+            n = n.next;
+		}
+		str = str.substring(0, str.length()-2);
+		System.out.println(str);
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = Integer.parseInt(input.nextLine());
+		for(int i = 0; i<n; i++) {
+			LinkedList list = new LinkedList();
+			while(input.hasNext()) {
+				String[] token = input.nextLine().split(" ");
+				if(token[0].equals("")) {
+					break;
+				}
+				switch(token[0]) {
+					case "push":
+					list.push(Integer.parseInt(token[1]));
+					list.print();
+					break;
+					case "pop":
+					if(list.pop()) {
+					list.print();
+					}
+					else {
+						System.out.println("empty");
+					}
+					break;
+					case "enqueue":
+					list.enqueue(Integer.parseInt(token[1]));
+					list.print();
+					break;
+				}
+			}
+		}
+
+	}
+
+}
