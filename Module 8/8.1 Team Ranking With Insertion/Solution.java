17cf4ae0a843b9c5a63c18305d5cc215cae7443c
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-02 19:10:01 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- Module 8/8.1 Team Ranking With Insertion/Solution.java
+++ Module 8/8.1 Team Ranking With Insertion/Solution.java
@@ -3,35 +3,36 @@ import java.util.Scanner;
 /**
  * Solution class reads input and creates object for TeamArray.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
 
-	private Solution() {
+    private Solution() {
 
-	}
-	/**
-	 * the TeamArray object is created and addTeam method is called.
-	 * Get method is also called which performs insertion sort.
-	 * Finally, the sorted team names are printed.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    }
+    /**
+     * the TeamArray object is created and addTeam method is called.
+     * Get method is also called which performs insertion sort.
+     * Finally, the sorted team names are printed.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
 
-		TeamArray teamArr = new TeamArray();
-		int c = 0;
-		while(input.hasNext()) {
-			String[] token = input.nextLine().split(",");
-			teamArr.addTeam(new Team(token[0], Integer.parseInt(token[1]),
-				Integer.parseInt(token[2]),
-				Integer.parseInt(token[3])));
-			c++;
-		}
-		teamArr.get();
+        TeamArray teamArr = new TeamArray();
+        int c = 0;
+        final int three = 3;
+        while (input.hasNext()) {
+            String[] token = input.nextLine().split(",");
+        teamArr.addTeam(new Team(token[0], Integer.parseInt(token[1]),
+                Integer.parseInt(token[2]),
+                Integer.parseInt(token[three])));
+            c++;
+        }
+        teamArr.get();
         System.out.println(teamArr.print());
 
-	}
+    }
 }
