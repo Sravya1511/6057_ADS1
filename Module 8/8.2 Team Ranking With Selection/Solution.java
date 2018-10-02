bcde74d7a02ae49df837422f2c48c228cf7165b3
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-02 19:40:53 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- Module 8/8.2 Team Ranking With Selection/Solution.java
+++ Module 8/8.2 Team Ranking With Selection/Solution.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
  * reads input.
  * Class for solution.
  */
-class Solution {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+
+    private Solution() {
+
+    }
     /**
      * The object for Team class is created.
      * and the method add adds the team into team Array.
@@ -13,18 +20,20 @@ class Solution {
      *
      * @param      args  The arguments
      */
-	public static void main(String[] args) {
+    public static void main(final String[] args) {
         /**
          * The sorting method is called.
          */
-		Scanner input = new Scanner(System.in);
-		TeamArray teamArr = new TeamArray();
-		while(input.hasNext()) {
-			String[] token = input.nextLine().split(",");
-			teamArr.addTeam(new Team(token[0], Integer.parseInt(token[1]), Integer.parseInt(token[2]), Integer.parseInt(token[3])));
-		}
+        Scanner input = new Scanner(System.in);
+        TeamArray teamArr = new TeamArray();
+        while (input.hasNext()) {
+            String[] token = input.nextLine().split(",");
+            teamArr.addTeam(new Team(token[0], Integer.parseInt(token[1]),
+                Integer.parseInt(token[2]),
+                Integer.parseInt(token[3])));
+        }
         teamArr.get();
         System.out.println(teamArr.print());
 
-	}
+    }
 }
