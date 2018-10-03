d79531bff26121a8c6b5474bba22b849cc569608
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-03 21:29:03 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module 9/9.1 Merge Sort Improvements/Solution.java
+++ Module 9/9.1 Merge Sort Improvements/Solution.java
@@ -1,95 +1,110 @@
-import java.util.Arrays;
-import java.util.Scanner;
-class Insertion {
-
-    public boolean less(Comparable a, Comparable b) {
-       return a.compareTo(b) < 0;
-   }
-   public void insertionSort(Comparable[] array) {
-    System.out.println("Insertion sort method invoked...");
-    int size = array.length;
-        for(int i = 0; i<size; i++) {
-            for(int j = i; j>0; j--) {
-                if(less(array[j], array[j-1])) {
-                    Comparable temp = 0;
-                    temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                }
-            }
-        }
-    }
-
-}
+import java.util.*;
 class MergeSort {
 
+    MergeSort() {
 
-    public boolean less(Comparable a, Comparable b) {
-    	return a.compareTo(b) < 0;
     }
+    /**
+     * Time complexity is O(N).
+     */
+    public void merge(Comparable[] array, Comparable[] auxilary, int low, int mid, int high) {
+        assert isSorted(array, low, mid);
+        assert isSorted(array, mid+1, high);
 
-    public boolean isSort(Comparable[] array) {
-        for(int i = 1; i<array.length; i++) {
-            if(!less(array[i-1], array[i])) {
-                return false;
-            }
+        int i = low;
+        int j = mid+1;
+        for (int k = low; k <= high; k++) {
+            if (i > mid)
+                auxilary[k] = array[j++];
+            else if (j > high)
+                auxilary[k] = array[i++];
+            else if (less(array[j], array[i]))
+                auxilary[k] = array[j++];
+            else
+                auxilary[k] = array[i++];
         }
-        return true;
+        assert isSorted(auxilary, low, high);
     }
-
-    public void sort(Comparable[] array, Comparable[] aux, int low, int high) {
-    	if (low >= high) {
-    		return;
-    	}
-    	int mid = low+(high-low)/2;
-        if(high-low <= 7) {
-        Insertion insertion = new Insertion();
-        insertion.insertionSort(array);
-        if(isSort(array)) {
-            System.out.println("Array is already sorted. So, skipped the call to merge...");
+    /**
+     * Time complexity is O(1)
+     */
+    private void sort(Comparable[] array, Comparable[] auxilary, int low, int high) {
+        if (high <= low + 7) {
+            insertionSort(auxilary, low, high);
+            System.out.println("Insertion sort method invoked...");
             return;
         }
+        int mid = low + (high - low) / 2;
+        sort(auxilary, array, low, mid);
+        sort(auxilary, array, mid+1, high);
+
+        if (!less(array[mid+1], array[mid])) {
+           for (int i = low; i <= high; i++) {
+            auxilary[i] = array[i];
+            }
+            System.out.println("Array is already sorted. So, skipped the call to merge...");
+           return;
         }
-
-        // if(mid-low < 7 && array.length >7) {
-        // Insertion insertion = new Insertion();
-        // // System.out.println("Insertion sort method invoked...");
-        // insertion.insertionSort(array, mid-low);
-
-        // }
-        // if(high-mid < 7 && array.length >7) {
-        // Insertion insertion = new Insertion();
-        // // System.out.println("Insertion sort method invoked...");
-        // insertion.insertionSort(array, high-mid);
-
-
-        // }
-        else {
-        sort(array, aux, low, mid);
-        sort(array, aux, mid+1, high);
-        merge(array, aux, low, mid, high);
-        }
-
-
+        merge(array, auxilary, low, mid, high);
     }
-
-    public void merge(Comparable[] array, Comparable[] aux, int low, int mid, int high) {
-    	for(int i = low; i<=high; i++) {
-    		aux[i] = array[i];
-    	}
-    	int i = low;
-    	int j = mid+1;
-    	for(int k = low; k<=high; k++) {
-    	if (i > mid)
-            array[k] = aux[j++];
-        else if (j > high)
-            array[k] = aux[i++];
-        else if (less(aux[j], aux[i]))
-            array[k] = aux[j++];
-        else  array[k] = aux[i++];
-    	}
-
+    /**
+     * Time complexity is O(1)
+     */
+    public void sort(Comparable[] a) {
+        Comparable[] aux = a.clone();
+        sort(aux, a, 0, a.length-1);
+        assert isSorted(a);
     }
+    /**
+     * Time complexity is O(N^2)
+     */
+    public void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j-1]); j--)
+                exch(a, j, j-1);
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    public void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    public boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+    /**
+     * Time complexity is O(1).
+     */
+    public boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    /**
+     * Time complexity is O(N)
+     */
+    public boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        return true;
+    }
+    /**
+     * Time complexity is O(N)
+     */
+    // public Object show(Object[] a) {
+    //     String s = "[";
+    //     int i;
+    //     for (i = 0; i < a.length - 1; i++) {
+    //         s += a[i] + ", ";
+    //     }
+    //     s = s + a[a.length - 1] + "]";
+    //     return s;
+    // }
 }
 
 class Solution {
@@ -100,7 +115,7 @@ class Solution {
         int size = token.length;
         Comparable[] aux = new Comparable[size];
         MergeSort merge = new MergeSort();
-        merge.sort(token, aux, 0, token.length-1);
+        merge.sort(token);
         System.out.println(Arrays.toString(token));
         System.out.println();
         }
