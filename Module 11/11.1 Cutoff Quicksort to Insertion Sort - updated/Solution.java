5252ced3cf6d1ecbece6e67ad7238041e0ff13fd
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-04 23:01:50 +0530

    "ads1_m11_a1_v2: 3/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ Module 11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -95,7 +95,7 @@ class Solution {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = Integer.parseInt(input.nextLine());
-        for(int i = 0; i<n*2; i++) {
+        for(int i = 0; i<n; i++) {
             QuickSort quick = new QuickSort();
             int cutOff = Integer.parseInt(input.nextLine());
             String[] array = input.nextLine().split(" ");
