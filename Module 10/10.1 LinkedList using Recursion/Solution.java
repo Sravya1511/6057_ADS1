9fc68a0ed7e3057136489dfc49f94b8ddba50a93
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-04 21:16:36 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Module 10/10.1 LinkedList using Recursion/Solution.java
+++ Module 10/10.1 LinkedList using Recursion/Solution.java
@@ -4,47 +4,47 @@ import java.util.Scanner;
  * reads input.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
+    /**
+     * Constructs the object.
+     */
 
-	private Solution() {
+    private Solution() {
 
-	}
-	/**
-	 * the object for linkedlist class is created.
-	 * The insertAt method and reverse methods are called.
-	 *
-	 * @param      args  The arguments
-	 */
+    }
+    /**
+     * the object for linkedlist class is created.
+     * The insertAt method and reverse methods are called.
+     *
+     * @param      args  The arguments
+     */
 
     public static void main(final String[] args) {
-    	LinkedList list = new LinkedList();
-    	Scanner input = new Scanner(System.in);
-    	while (input.hasNext()) {
-    		String[] token = input.nextLine().split(" ");
-    		switch (token[0]) {
-    			case "insertAt":
-    			int size = list.size();
-    			int position = Integer.parseInt(token[1]);
-    			if (position < 0 || position > size) {
-    			System.out.println("Can't insert at this position.");
-    			} else {
+        LinkedList list = new LinkedList();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String[] token = input.nextLine().split(" ");
+            switch (token[0]) {
+                case "insertAt":
+                int size = list.size();
+                int position = Integer.parseInt(token[1]);
+                if (position < 0 || position > size) {
+                System.out.println("Can't insert at this position.");
+                } else {
                 list.insertAt(position, Integer.parseInt(token[2]));
-    			System.out.println(list.print());
-    			}
-    			break;
-    			case "reverse":
-    			if (list.size() == 0) {
-    				System.out.println("No elements to reverse.");
-    			} else {
-    				list.reverse();
-    				System.out.println(list.print());
-    			}
-    			break;
-    			default:
-    			break;
-    		}
-    	}
+                System.out.println(list.print());
+                }
+                break;
+                case "reverse":
+                if (list.size() == 0) {
+                    System.out.println("No elements to reverse.");
+                } else {
+                    list.reverse();
+                    System.out.println(list.print());
+                }
+                break;
+                default:
+                break;
+            }
+        }
     }
 }
