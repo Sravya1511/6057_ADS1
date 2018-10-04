8f0460c75b55c910e86f91e0c2d78140e68eaf6b
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-04 17:12:21 +0530

    "ads1_m10_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Module 10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+/**
+ * Class for node.
+ * Node class has two attributes.
+ * Data of type int.
+ * next of type Node
+ */
+class Node {
+	int data;
+	Node next;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      data  The data of type integer.
+	 */
+
+	Node(int data) {
+	this.data = data;
+	next = null;
+	}
+}
+/**
+ * List of linkeds.
+ */
+class LinkedList {
+	Node head;
+	Node tail;
+	int size = 0;
+	int count = 0;
+
+	public void insertAt(int pos, int data) {
+		size++;
+
+		Node newNode = new Node(data);
+		if (head == null) {
+            head = newNode;
+            return;
+		}
+		Node n = head;
+		if(count == 0 && pos == 0) {
+			newNode.next = head;
+			head = newNode;
+			return;
+		}
+		if(pos == 0) {
+			newNode.next = n.next;
+			n.next = newNode;
+			return;
+		}
+		else {
+            n = n.next;
+            count++;
+            insertAt(pos-1, data);
+		}
+	}
+
+	public int size() {
+		return size;
+	}
+
+
+
+	public String print() {
+    	Node n = head;
+    	String result = "";
+    	while(n!=null) {
+    		result += n.data+", ";
+    		n = n.next;
+    	}
+    	return result.substring(0, result.length()-2);
+    }
+}
+class Solution {
+
+    public static void main(String[] args) {
+    	LinkedList list = new LinkedList();
+    	Scanner input = new Scanner(System.in);
+    	while(input.hasNext()) {
+    		String[] token = input.nextLine().split(" ");
+    		switch (token[0]) {
+    			case "insertAt":
+    			int size = list.size();
+    			int position = Integer.parseInt(token[1]);
+    			if(position <0 || position>size) {
+    				System.out.println("Can't insert at this position.");
+    			}
+    			else {
+                    list.insertAt(position, Integer.parseInt(token[2]));
+    			    System.out.println(list.print());
+    			}
+
+    			break;
+    			case "reverse":
+    			if(list.size() == 0) {
+    				System.out.println("No elements to reverse.");
+    			}
+
+    			break;
+    			default:
+    			break;
+
+    		}
+    	}
+    }
+}
