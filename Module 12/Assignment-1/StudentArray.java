c78d12430251f9f1d2e12db840833a202ce8b97d
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-06 13:12:56 +0530

    "ads1_m12_a1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 12/Assignment-1/StudentArray.java
+++ Module 12/Assignment-1/StudentArray.java
@@ -1,19 +1,48 @@
+/**
+ * Class for student array.
+ */
 class StudentArray {
 	Student[] studentArray;
 	int size;
 
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { int n - size of student array }
+     */
 	StudentArray(int n) {
 		studentArray = new Student[n];
 		size = 0;
 	}
-
+    /**
+     * Adds a student.
+     *
+     * @param      student  The student of type student
+     */
 	public void addStudent(Student student) {
 		studentArray[size++] = student;
 	}
+	/**
+	 * Gets the array.
+	 *
+	 * @return     The array of student type.
+	 */
 
 	public Student[] getArray(){
 		return studentArray;
 	}
+	/**
+	 * Gets the reservation array.
+	 *
+	 * @param      vacancies      The vacancies of type int.
+	 * @param      meritArray     The merit array of type studnet.
+	 * @param      openVacancies  The open vacancies of type int
+	 * @param      bcVacancies    The bc vacancies of type int.
+	 * @param      scVacancies    The screen vacancies of type int.
+	 * @param      stVacancies    The st vacancies of type int.
+	 *
+	 * @return     The reservation array.
+	 */
 
 	public Student[] getReservationArray(int vacancies, Student[] meritArray, int openVacancies, int bcVacancies, int scVacancies, int stVacancies) {
 		Student[] res;
@@ -22,40 +51,39 @@ class StudentArray {
 		int z = 0;
 		res = new Student[30];
 		int count = 0;
-		for(int i = 0; i<meritArray.length; i++) {
-			if(meritArray[i].getReservation().equals("ST"))
-                 x++;
-            if(meritArray[i].getReservation().equals("SC"))
-                 y++;
-            if(meritArray[i].getReservation().equals("Bc"))
-                 z++;
+		for (int i = 0; i < meritArray.length; i++) {
+			if(meritArray[i].getReservation().equals("ST")) {
+				 x++;
+			}
+            if(meritArray[i].getReservation().equals("SC")) {
+            	y++;
+            }
+            if(meritArray[i].getReservation().equals("Bc")) {
+            	z++;
+            }
+
 		}
-		for(int i = 0; i<openVacancies; i++) {
+		for (int i = 0; i < openVacancies; i++) {
 			res[count++] = meritArray[i];
 		}
         int a = 0;
         int b = 0;
         int c = 0;
-		for(int i = openVacancies; i<meritArray.length;i++) {
+		for (int i = openVacancies; i < meritArray.length; i++) {
 			if(meritArray[i].getReservation().equals("ST") && a!=stVacancies) {
                 res[count++] = meritArray[i];
                 a++;
-			}
-			else if(meritArray[i].getReservation().equals("SC") && b!=scVacancies) {
+			} else if(meritArray[i].getReservation().equals("SC") && b != scVacancies) {
                 res[count++] = meritArray[i];
                 b++;
-			}
-			else if(meritArray[i].getReservation().equals("BC") && c!=bcVacancies) {
+			} else if(meritArray[i].getReservation().equals("BC") && c != bcVacancies) {
                 res[count++] = meritArray[i];
                 c++;
-			}
-			else if(x < stVacancies) {
+			} else if(x < stVacancies) {
 				res[count++] = meritArray[i];
-			}
-			else if(y < scVacancies) {
+			} else if(y < scVacancies) {
 				res[count++] = meritArray[i];
-			}
-			else if(z < bcVacancies) {
+			} else if(z < bcVacancies) {
 				res[count++] = meritArray[i];
 			}
 		}
