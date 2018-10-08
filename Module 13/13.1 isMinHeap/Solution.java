ae907b4f520e388851e293cbcc8f6abeaea42de9
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-08 17:21:54 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 13/13.1 isMinHeap/Solution.java
+++ Module 13/13.1 isMinHeap/Solution.java
@@ -59,15 +59,15 @@ class Solution {
 				// System.out.println(Arrays.toString(tokens));
 				// System.out.println(tokens.length);
 
-				String[] res = new String[tokens.length+1];
-				int m = 1;
-				for(int k = 0; k<tokens.length; k++) {
-                      res[m] = tokens[k];
-                      m++;
-				}
+				// String[] res = new String[tokens.length+1];
+				// int m = 1;
+				// for(int k = 0; k<tokens.length; k++) {
+    //                   res[m] = tokens[k];
+    //                   m++;
+				// }
 				// System.out.println(Arrays.toString(res));
 				// System.out.println(res.length);
-				MinPQ<String> stringArray = new MinPQ<String>(res, res.length);
+				MinPQ<String> stringArray = new MinPQ<String>(tokens, tokens.length);
 				System.out.println(stringArray.isMinHeap());
 				break;
 
