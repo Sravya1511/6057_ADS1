db51a9cd903770eb6920d5d86caffcf1eb04472c
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-08 20:55:55 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 13/13.1 isMinHeap/Solution.java
+++ Module 13/13.1 isMinHeap/Solution.java
@@ -29,7 +29,7 @@ class MinPQ<E extends Comparable<E>> {
 	public boolean isMinHeap() {
 		int n = pq.length - 1;
           while(n > 1) {
-          	if (pq[n].compareTo(pq[n/2]) >= 0) {
+          	if (pq[n].compareTo(pq[n / 2]) >= 0) {
           		n = -1;
           	} else {
           		return false;
@@ -42,26 +42,38 @@ class MinPQ<E extends Comparable<E>> {
  * Class for solution.
  */
 
-class Solution {
-	public static void main(String[] args) {
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * reads input.
+	 * creates object for minpq class.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		String type = input.nextLine();
 		int n = Integer.parseInt(input.nextLine());
-		switch(type) {
+		switch (type) {
 			case "String":
 			for (int i = 0; i < n; i++) {
 
 				String[] tokens = input.nextLine().split(",");
 
-				String[] res = new String[tokens.length+1];
+				String[] res = new String[tokens.length + 1];
 				// int m = 1;
 				for (int k = 0; k < tokens.length; k++) {
                       res[k + 1] = tokens[k];
 
 				}
 
-				MinPQ<String> stringArray = new MinPQ<String>(res);
-				System.out.println(stringArray.isMinHeap());
+	MinPQ<String> stringArray = new MinPQ<String>(res);
+		System.out.println(stringArray.isMinHeap());
 			}
 			break;
 			case "Integer":
@@ -74,8 +86,8 @@ class Solution {
                       res[k + 1] = Integer.parseInt(tokens[k]);
 
 				}
-				MinPQ<Integer> intArray = new MinPQ<Integer>(res);
-				System.out.println(intArray.isMinHeap());
+		MinPQ<Integer> intArray = new MinPQ<Integer>(res);
+		System.out.println(intArray.isMinHeap());
 			}
 			break;
 			case "Float":
@@ -88,11 +100,9 @@ class Solution {
 					// System.out.println("hi");
 					return;
 				}
-				Float[] res = new Float[tokens.length+1];
+				Float[] res = new Float[tokens.length + 1];
 				int m = 1;
-
-                if (tokens.length != 0)
-					for(int k = 0; k < tokens.length; k++) {
+					for (int k = 0; k < tokens.length; k++) {
                       res[m] = Float.parseFloat(tokens[k]);
                       m++;
 				}
@@ -109,8 +119,8 @@ class Solution {
                       res[m] = Double.parseDouble(tokens[k]);
                       m++;
 				}
-				MinPQ<Double> doubleArray = new MinPQ<Double>(res);
-				System.out.println(doubleArray.isMinHeap());
+			MinPQ<Double> doubleArray = new MinPQ<Double>(res);
+			System.out.println(doubleArray.isMinHeap());
 			}
 			break;
 			default:
